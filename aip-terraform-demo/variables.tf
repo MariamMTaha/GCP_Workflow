530d0dcdfbe9fd330605e694678d3b2d18550ce9
MariamMTaha <mariamtaha@aucegypt.edu>
2026-09-22 23:15:51 +0300

    Update variables.tf

--- aip-terraform-demo/variables.tf
+++ aip-terraform-demo/variables.tf
@@ -1,7 +1,7 @@
 variable "project_id" {
   description = "GCP Project ID"
   type        = string
-  default     = "emea-gcp-ema-tt-0c8aa-sbx-1"
+  default     = "emea-gcp-ema-tt-605a5-sbx-1"
 }
 variable "region" {
   description = "GCP Region"
